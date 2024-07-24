59299eb22b5429328569ac23e77bd15c4a70bdb0
Sirrobert-hub <brianrobert4361@gmail.com>
2024-07-24 13:06:42 +0300

    Update Python.py

--- Python.py
+++ Python.py
@@ -10,4 +10,4 @@ age = input("How old are you? ")
 location = input("Where do you live? ")
 
 # Print out a personalized message
-print(f"Hello {name}, you are {age} years old and live in {location}.")
+print(f"Hello {name}, you are {age} years old and live in {location}."
